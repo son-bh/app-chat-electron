22109be15e7df106daec13e8c98eff3b7958be28
son-bh <sonhong.bui15@gmail.com>
2025-08-22 16:44:17 +0700

    Update path locales

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,11 +1,11 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 export const supportedLngs = {
-  en: 'English',
-  vi: 'Việt Nam',
+  en: "English",
+  vi: "Việt Nam",
 };
 
 i18n
@@ -13,9 +13,12 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: "en",
     supportedLngs: Object.keys(supportedLngs),
     debug: true,
+    backend: {
+      loadPath: "./locales/{{lng}}/{{ns}}.json",
+    },
     interpolation: {
       escapeValue: false,
     },
