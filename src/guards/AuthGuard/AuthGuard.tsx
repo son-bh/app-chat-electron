d49b2a6d6bb472cd3cc19939111949cd33518d50
son-bh <sonhong.bui15@gmail.com>
2025-08-26 15:28:16 +0700

    CI/CD: Fix version from tag

--- src/guards/AuthGuard/AuthGuard.tsx
+++ src/guards/AuthGuard/AuthGuard.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { CookiesStorage } from '../../shared/utils/cookie-storage';
-import { PATH_NAME } from '../../configs';
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { CookiesStorage } from "../../shared/utils/cookie-storage";
+import { PATH_NAME } from "../../configs";
 
 interface IAuthGuard {
   children?: React.ReactNode;
@@ -9,8 +9,16 @@ interface IAuthGuard {
 
 const AuthGuard = ({ children }: IAuthGuard) => {
   const isAuth = CookiesStorage.getAccessToken();
+  const location = useLocation();
 
-  if (!isAuth) return <Navigate to={PATH_NAME.LOGIN} replace={true} />;
+  if (!isAuth)
+    return (
+      <Navigate
+        to={PATH_NAME.LOGIN}
+        replace={true}
+        state={{ from: location }}
+      />
+    );
 
   return <>{children}</>;
 };
